b80f9b75bc6d9b80f19076fd112dd4e2170df1be
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-06-15 10:54:33 -0300

    Create styles.js

---
+++ src/components/Header/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	display: flex;
+	width: 100%;
+	height: 200px;
+	background: #f2f2f2;
+`;
+
+export const Content = styled.div`
+	background: #f2f2f2;
+	text-transform: uppercase;
+	display: flex;
+
+	ul {
+		display: flex;
+	}
+`;
