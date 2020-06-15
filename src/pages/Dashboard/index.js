e94379b6df62524b4b462fd7fe08ff0a9fc0ecd0
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-06-15 10:54:23 -0300

    Create index.js

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header';
+import * as S from './styles';
+
+export default function Dashboard() {
+	return (
+		<>
+			<Header />
+			<S.Container>
+				<h1>Dashboard</h1>
+			</S.Container>
+		</>
+	);
+}
